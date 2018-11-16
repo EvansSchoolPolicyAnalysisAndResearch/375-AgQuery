a9c74a7a1c6ff8993d780a595737bda5e96e4bcf
Sammi Kiel <sammi@muelnet.org>
2018-11-16 13:26:43 -0800

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""
+Copyright 2018 Evans Policy Analysis and Research Group (EPAR)
+
+This project is licensed under the 3-Clause BSD License. Please see the 
+license.txt file for more information.
+"""
---
+++ config/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#dev-config.py
+
+"""
+Copyright 2018 Evans Policy Analysis and Research Group (EPAR)
+
+This project is licensed under the 3-Clause BSD License. Please see the 
+license.txt file for more information.
+"""
+# Enable Debugging
+DEBUG = True
