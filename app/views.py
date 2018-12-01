57c7374a4d468612dc5f2d8fbd87b1f33090877a
Sammi Kiel <sammi@muelnet.org>
2018-11-30 16:29:41 -0800

    Initial working version

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#app/views.py
 
 """
 Copyright 2018 Evans Policy Analysis and Research Group (EPAR)
@@ -6,3 +6,14 @@ Copyright 2018 Evans Policy Analysis and Research Group (EPAR)
 This project is licensed under the 3-Clause BSD License. Please see the 
 license.txt file for more information.
 """
+
+from flask import render_template
+from app import app
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+@app.route('/login')
+def index():
+	return render_template("login.html")
