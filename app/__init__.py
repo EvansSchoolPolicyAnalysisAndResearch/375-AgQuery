5c096a2efe073c60c075e477c86a2c8b3500be57
Sammi Kiel <sammi@muelnet.org>
2018-11-30 16:29:18 -0800

    Initial working version

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,22 @@
-#!/usr/bin/python3
+#app/init.py
 
 """
 Copyright 2018 Evans Policy Analysis and Research Group (EPAR)
 
-This project is licensed under the 3-Clause BSD License. Please see the 
+This project is licensed under the 3-Clause BSD License. Please see
 license.txt file for more information.
 """
-#app/init.py
+
+
+from flask import Flask
+
+# initialize the app
+app = Flask(__name__, instance_relative_config=True)
+
+# Load the views
+from app import views
+
+# Load the Config file
+app.config.from_object('config')
+
+
