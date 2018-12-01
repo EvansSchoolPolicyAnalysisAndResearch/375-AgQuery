db8f2d52ecfdc293f3143fcb6564d39a88247825
Sammi Kiel <sammi@muelnet.org>
2018-11-30 16:30:10 -0800

    Initial working version

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
-#!/usr/bin/python3
+#run.py
 
 """
 Copyright 2018 Evans Policy Analysis and Research Group (EPAR)
 
-This project is licensed under the 3-Clause BSD License. Please see the 
+This project is licensed under the 3-Clause BSD License. Please see the
 license.txt file for more information.
 """
 
+from app import app
+
+if __name__ == '__main__':
+    app.run()
