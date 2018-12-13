53c011f5d404a3a23aaea9a1657d9d58e44640f7
Sammi Kiel <sammi@muelnet.org>
2018-12-12 16:21:59 -0800

    updated to better format

--- config.py
+++ config.py
@@ -6,5 +6,19 @@ Copyright 2018 Evans Policy Analysis and Research Group (EPAR)
 This project is licensed under the 3-Clause BSD License. Please see the 
 license.txt file for more information.
 """
+from dotenv import load_dotenv
+import os
+
+local_path = os.path.dirname(os.path.abspath(__file__))
+dotenv_path = os.path.join(local_path, '.env')
+sqlite_path = os.path.join(local_path, 'data', 'Estimates.db')
+load_dotenv(dotenv_path)
+###############################################################################
+#      Modify this for the local settings and configuration needs of the      #
+#                             instance in question                            #
+###############################################################################
 # Enable Debugging
 DEBUG = True
+SQLALCHEMY_DATABASE_URI='sqlite:///%s' % (sqlite_path)
+SQLALCHEMY_TRACK_MODIFICATIONS=False
+
