6e0b7f92f73ef53a2cd421be4c80f287d7312f98
Sammi Kiel <sammi@muelnet.org>
2018-12-13 17:27:24 -0800

    Added code for handling years and better filtering on geography

--- app/views.py
+++ app/views.py
@@ -8,7 +8,9 @@ license.txt file for more information.
 """
 from app import app
 from sqlalchemy.sql import select
+from sqlalchemy import and_, or_
 from flask import render_template,request
+import ast
 from app.database import db_session
 from app.models import Estimates
 
@@ -17,21 +19,29 @@ def index():
 	"""
 	"""
 	indicators=[]
+	yrs = []
+	geos = []
+
 	geography = [r.geography for r in 
 		db_session.query(Estimates.geography).distinct()]
 	indicatorCategory = [r.indicatorCategory for r in
 			db_session.query(Estimates.indicatorCategory).distinct()]
+	years = [r.year for r in 
+			db_session.query(Estimates.year).distinct()]
+
 	goDisabled = True
 	if request.form:
 		cats = request.form.getlist('indicatorCategory')
 		geos = request.form.getlist('geography')
-		if cats and geos:
+		yrs = request.form.getlist('years')
+		if cats:
 			goDisabled = False
 			indicators = [r.indicatorName for r in
-				db_session.query(Estimates.indicatorName).distinct().filter(Estimates.indicatorCategory.in_(cats),
-					Estimates.geography.in_(geos))]
+				db_session.query(Estimates.indicatorName).distinct().filter(Estimates.indicatorCategory.in_(cats))]
 	
-	return render_template("index.html",indicators=indicators, geography=geography, indicatorCategory=indicatorCategory, goDisabled=goDisabled)
+	return render_template("index.html",indicators=indicators, 
+		geography=geography, indicatorCategory=indicatorCategory, 
+		goDisabled=goDisabled, years=years,yrs=yrs, geos=geos)
 
 @app.route('/login')
 def login():
@@ -43,15 +53,25 @@ def login():
 def results():
 	"""
 	"""
+	indicators = []
 	if request.method == "POST":
-		indicatorNames = request.values
-		indicators = db_session.query(
-			Estimates).filter(
-				Estimates.indicatorName.in_(indicatorNames))
+		geos = ast.literal_eval(request.form.get('geography'))
+		yrs = ast.literal_eval(request.form.get('years'))
+		indicatorNames = request.values.to_dict()
+		# Remove years and geography from the list returned to indicator names
+		del indicatorNames ['years']
+		del indicatorNames ['geography'] 
+		filt = Estimates.indicatorName.in_(indicatorNames)
 
-		for ind in indicators:
-			pass
-	return render_template("results.html", indicators=indicators)
+		if geos:
+			filt = and_(filt, Estimates.geography.in_(geos))
+		if yrs:
+			filt = and_(filt, Estimates.year.in_(yrs))
+		indicators = db_session.query(Estimates).filter(filt)
+		#for ind in indicators:
+		#	pass
+
+	return render_template("results.html", indicators=indicators, geos=indicatorNames)
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
