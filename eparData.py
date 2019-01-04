18e5f0b6c76bec40980162d1501274abfb58c29e
Sammi Kiel <sammi@muelnet.org>
2019-01-04 15:39:06 -0800

    Modified to be inline with Gunicorn expectation

--- eparData.py
+++ eparData.py
@@ -1,4 +1,4 @@
-#run.py
+#eparData.py
 
 """
 Copyright 2018 Evans Policy Analysis and Research Group (EPAR)
@@ -6,7 +6,9 @@ Copyright 2018 Evans Policy Analysis and Research Group (EPAR)
 This project is licensed under the 3-Clause BSD License. Please see the
 license.txt file for more information.
 """
-from app import app
+# Note the as application is to conform with the Gunicorn expected use of 
+# application rather than app
+from app import app as application
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
