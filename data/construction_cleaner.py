097057b55a599308d02626773ef4d862773a21cf
penny k <790306+s-kiel@users.noreply.github.com>
2019-05-27 12:38:09 -0700

    Improved functionality of the data cleaning scripts
    Added the ability to set arbitrary file names for cleaning. Also moved the main work
    of the file into a function so it can be called from a unified automation script in
    the future.

--- data/construction_cleaner.py
+++ data/construction_cleaner.py
@@ -1,5 +1,14 @@
+#!/usr/bin/python3
+
 #data/construction_cleaner.py
 """
+This script modifies the indicator construction sheet of EPAR's Agricultural
+Indicator Estimate Project into a format which can imported into the indicator
+query tool's database. It requires said sheet to be exported as a CSV. It 
+ouputs a cleaned versions of the file to construction_cleaned.csv and 
+construction_countries_cleaned.csv
+"""
+__copyright__ = """
 Copyright 2019 Evans Policy Analysis and Research Group (EPAR)
 
 This project is licensed under the 3-Clause BSD License. Please see the 
@@ -7,6 +16,10 @@ license.txt file for more information.
 """
 
 import csv
+import argparse
+INDCOLUMN = 0
+
+instruments = ("ETH ESS Wave 1", "ETH ESS Wave 2", "ETH ESS Wave 3", "NGA GHSP Wave 1", "NGA GHSP Wave 2", "NGA GHSP Wave 3", "TZA NPS Wave 1", "TZA NPS Wave 2", "TZA NPS Wave 3", "TZA NPS Wave 4")
 
 def make_id_counter():
 	next_id = 1
@@ -17,38 +30,53 @@ def make_id_counter():
 		return id_num
 	return id_counter
 
-instruments = ("ETH ESS Wave 1", "ETH ESS Wave 2", "ETH ESS Wave 3", "NGA GHSP Wave 1", "NGA GHSP Wave 2", "NGA GHSP Wave 3", "TZA NPS Wave 1", "TZA NPS Wave 2", "TZA NPS Wave 3", "TZA NPS Wave 4")
-INDCOLUMN = 0
 
-# Clean the construction csv exported from the Excel File
-with open('construction.csv', newline='') as infile:
-	with open('construction_cleaned.csv', 'w', newline='') as outfile:
-		with open('construction_countries_cleaned.csv', 'w', newline='') as cntry:
-			rdr = csv.reader(infile, delimiter=',', quotechar='"', 
-				quoting=csv.QUOTE_NONNUMERIC)
-			wrtr = csv.writer(outfile, delimiter=',', quotechar='"', 
-				quoting=csv.QUOTE_NONNUMERIC)
-			cntry_wrtr = csv.writer(cntry, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-			get_id = make_id_counter()
+def clean_construction_decisions(filename):
+	# Clean the construction csv exported from the Excel File
+	with open(filename, newline='') as infile:
+		with open('construction_cleaned.csv', 'w', newline='') as outfile:
+			with open('construction_countries_cleaned.csv', 'w', newline='') as cntry:
+				rdr = csv.reader(infile, delimiter=',', quotechar='"', 
+					quoting=csv.QUOTE_NONNUMERIC)
+				wrtr = csv.writer(outfile, delimiter=',', quotechar='"', 
+					quoting=csv.QUOTE_NONNUMERIC)
+				cntry_wrtr = csv.writer(cntry, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+				get_id = make_id_counter()
 
-			for r,row in enumerate(rdr):
-				# Postgres does not like having headers, so this removes them
-				if r == 0:
-					continue
-				row_stub = row[0:15]
-				
-				# Clean the data up a bit
-				for i,elem in enumerate(row_stub) :
-					if elem == "0 ":
-						row_stub[i] = 0
-					elif elem == "1": 
-						row_stub[i] = 1
-					elif type(elem) == str:
-						row_stub[i] = elem.strip()
-					else:
-						row_stub[i] = elem
-				wrtr.writerow(row_stub)
-				for i,inst in enumerate(instruments):
-					# Note Row Stub 2 is the varnamestem
-					cntry_row = [get_id(), inst, row[i+15], row_stub[INDCOLUMN]]
-					cntry_wrtr.writerow(cntry_row)
+				for r,row in enumerate(rdr):
+					# Postgres does not like having headers, so this removes them
+					if r == 0:
+						continue
+					# Row Stub is the the elements needed for the non-country
+					# specific information
+					row_stub = row[0:15]
+					
+					# Clean the data up a bit, removig excess spaces, making
+					# sure numbers are viewed as numbers not strings, etc.
+					for i,elem in enumerate(row_stub) :
+						if elem == "0 ":
+							row_stub[i] = 0
+						elif elem == "1": 
+							row_stub[i] = 1
+						elif type(elem) == str:
+							row_stub[i] = elem.strip()
+						else:
+							row_stub[i] = elem
+					wrtr.writerow(row_stub)
+					for i,inst in enumerate(instruments):
+						# Note Row Stub 2 is the varnamestem
+						cntry_row = [get_id(), inst, row[i+15], row_stub[INDCOLUMN]]
+						cntry_wrtr.writerow(cntry_row)
+
+
+if __name__ == "__main__":
+	
+	# Get commandline arguments
+	pars = argparse.ArgumentParser( description = __doc__,
+		epilog = __copyright__,
+		formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+	pars.add_argument( 'file', nargs='?', default='construction.csv', 
+		help = "The name of the file containing the construction decisions")
+	args = pars.parse_args()
+
+	clean_construction_decisions(args.file)
--- data/estimates_cleaner.py
+++ data/estimates_cleaner.py
@@ -1,61 +1,82 @@
-#data/csv_cleaner.py
+#!/usr/bin/python3
+# data/estimates_cleaner.py
+
 """
+This script modifies the estimates sheet of EPAR's Agricultural Indicator
+Estimate Project into a format which can imported into the indicator query
+tool's database. It requires said sheet to be exported as a CSV. It ouputs a 
+cleaned version of the script to estimates_cleaned.csv
+"""
+import csv, argparse
+
+__copyright__ = """\
 Copyright 2019 Evans Policy Analysis and Research Group (EPAR)
 
 This project is licensed under the 3-Clause BSD License. Please see the 
 license.txt file for more information.
 """
 
-import csv
+
+
 INDICATORCOL=6
 CROPCOL=8
-headers = ["id", "geography", "survey", "instrument", "year", "indicatorCategory", "indicatorName", "units", "cropDisaggregation", "genderDisaggregation", "farmSizeDisaggregation", "ruralortotal", "subpopulation", "currencyConversion", "indicatorLevel", "weight", "variableName", "mean", "se", "sd", "p25", "median", "p75", "minim", "maxim", "n", "nover30"]
 
-# Clean the estimates csv exported from the Excel File 
-with open('estimates.csv', newline='') as infile:
-	with open('estimates_cleaned.csv', 'w', newline='') as outfile:
-		rdr = csv.reader(infile, delimiter=',', quotechar='"', 
-			quoting=csv.QUOTE_NONNUMERIC)
-		wrtr = csv.writer(outfile, delimiter=',', quotechar='"', 
-			quoting=csv.QUOTE_MINIMAL)
-		for r,row in enumerate(rdr):
-			# Postgres does not like having headers, so this removes them
-			if r == 0:
-				continue
-			# Add the ID to the row
-			row.insert(0, r)
-			# Clean the elements of the row
-			for i,elem in enumerate(row) :
-				if elem == "0 ":
-					row[i] = 0
-				elif elem == "1": 
-					row[i] = 1
-				elif type(elem) == str:
-					row[i] = elem.strip()
-				else:
-					row[i] = elem
-			if ' - large ruminants, small ruminants, poultry' in row[INDICATORCOL]:
-				row[INDICATORCOL] = row[INDICATORCOL].replace(' - large ruminants, small ruminants, poultry', '')
-				row[CROPCOL] = "All livestock"
-			elif ' - large ruminants' in row[INDICATORCOL]:
-				row[INDICATORCOL] = row[INDICATORCOL].replace(' - large ruminants', '')
-				row[CROPCOL] = "Large ruminants"
-			elif ' - small ruminants' in row[INDICATORCOL]:
-				row[INDICATORCOL] = row[INDICATORCOL].replace(' - small ruminants', '')
-				row[CROPCOL] = "Small ruminants"
-			elif ' - poultry' in row[INDICATORCOL]:
-				row[INDICATORCOL] = row[INDICATORCOL].replace(' - poultry', '')
-				row[CROPCOL] = "Poultry"
-			elif ' - cows' in row[INDICATORCOL]:
-				row[INDICATORCOL] = row[INDICATORCOL].replace(' - cows', '')
-				row[CROPCOL] = "Cows"
-			elif ' - buffalos' in row[INDICATORCOL]:
-				row[INDICATORCOL] = row[INDICATORCOL].replace(' - buffalos', '')
-				row[CROPCOL]="Buffalos"
-			elif row[INDICATORCOL] == "Milk productivity":
-				row[CROPCOL] = "Large ruminants"
+"""headers = ["id", "geography", "survey", "instrument", "year", "indicatorCategory", "indicatorName", "units", "cropDisaggregation", "genderDisaggregation", "farmSizeDisaggregation", "ruralortotal", "subpopulation", "currencyConversion", "indicatorLevel", "weight", "variableName", "mean", "se", "sd", "p25", "median", "p75", "minim", "maxim", "n", "nover30"]
+"""
+def clean_estimates(filename):
+	# Clean the estimates csv exported from the Excel File 
+	with open(filename, newline='') as infile:
+		with open('estimates_cleaned.csv', 'w', newline='') as outfile:
+			rdr = csv.reader(infile, delimiter=',', quotechar='"', 
+				quoting=csv.QUOTE_NONNUMERIC)
+			wrtr = csv.writer(outfile, delimiter=',', quotechar='"', 
+				quoting=csv.QUOTE_MINIMAL)
+			for r,row in enumerate(rdr):
+				# Postgres does not like having headers, so this removes them
+				if r == 0:
+					continue
+				# Add the ID to the row
+				row.insert(0, r)
+				# Clean the elements of the row
+				for i,elem in enumerate(row) :
+					if elem == "0 ":
+						row[i] = 0
+					elif elem == "1": 
+						row[i] = 1
+					elif type(elem) == str:
+						row[i] = elem.strip()
+					else:
+						row[i] = elem
+				if ' - large ruminants, small ruminants, poultry' in row[INDICATORCOL]:
+					row[INDICATORCOL] = row[INDICATORCOL].replace(' - large ruminants, small ruminants, poultry', '')
+					row[CROPCOL] = "All livestock"
+				elif ' - large ruminants' in row[INDICATORCOL]:
+					row[INDICATORCOL] = row[INDICATORCOL].replace(' - large ruminants', '')
+					row[CROPCOL] = "Large ruminants"
+				elif ' - small ruminants' in row[INDICATORCOL]:
+					row[INDICATORCOL] = row[INDICATORCOL].replace(' - small ruminants', '')
+					row[CROPCOL] = "Small ruminants"
+				elif ' - poultry' in row[INDICATORCOL]:
+					row[INDICATORCOL] = row[INDICATORCOL].replace(' - poultry', '')
+					row[CROPCOL] = "Poultry"
+				elif ' - cows' in row[INDICATORCOL]:
+					row[INDICATORCOL] = row[INDICATORCOL].replace(' - cows', '')
+					row[CROPCOL] = "Cows"
+				elif ' - buffalos' in row[INDICATORCOL]:
+					row[INDICATORCOL] = row[INDICATORCOL].replace(' - buffalos', '')
+					row[CROPCOL]="Buffalos"
+				elif row[INDICATORCOL] == "Milk productivity":
+					row[CROPCOL] = "Large ruminants"
 
-			wrtr.writerow(row)
+				wrtr.writerow(row)
+if __name__ == "__main__":
 
+	pars = argparse.ArgumentParser(description = __doc__, 
+		epilog = __copyright__,
+		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+	pars.add_argument( 'file', nargs='?', default='estimates.csv', 
+		help = "the name of the csv file containing the indicator estimates")
+	args = pars.parse_args()
 
+	clean_estimates(args.file)
