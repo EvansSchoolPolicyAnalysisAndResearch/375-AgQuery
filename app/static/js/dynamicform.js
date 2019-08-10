603faefbefe806551a0c8b3c806fb6d45bbf9515
penny k <790306+s-kiel@users.noreply.github.com>
2019-08-09 17:14:10 -0700

    initial version of the no categories, filter version of the site

--- app/static/js/dynamicform.js
+++ app/static/js/dynamicform.js
@@ -1,53 +1,103 @@
-function SelectAll(name) {
-	var boxes =  document.getElementsByClassName(name);
-	for(i = 0; i < boxes.length; i++) {
-		boxes[i].checked = true;
-	}
-}
+
+/**
+ * Creates an object which contains the functions for manipulating the web
+ *
+ * @author     s. kiel
+ * @copyright  Evans School Policy Analysis and Research Group 2019
+ *
+ * @type       {Object} Custom object to prevent global namespace pollution
+ */
+var AgQuery = new Object({
 
 
-function ClearAll(name) {
-	var boxes = document.getElementsByClassName(name);
-	for(i = 0; i < boxes.length; i++) {
-		boxes[i].checked = false;
-	}
-}
+	/**
+	 * { function_description }
+	 *
+	 * @param      String  classname  The classname of the 
+	 */
+	SelectAll: function(classname)  {
+		// Finds all of the check boxes with the given name.
+		var boxes =  document.getElementsByClassName(classname);
 
-function ToggleSelect(element) {
-	if(element.checked == true){
-		SelectAll(element.id);
-	} else {
-		ClearAll(element.id);
-	}
-}
+		for (let box of boxes) {
+			if (box.offsetWidth != 0 && box.offsetHeight !=0){
+				box.checked = true;
+			}
+		}
+	},
 
-function EnableButtons() {
-	var indicators	= document.getElementsByName("indicator");
-	var geoyears	= document.getElementsByName("geo_year");
-	var hasind		= false;
-	var hasgeo		= false;
-	var submitters	= document.getElementsByClassName("submitter");
-	for(i=0; i < indicators.length; i++) {
-		if(indicators[i].checked == true){
-			hasind = true;
-			break;
+	ClearAll: function(classname) {
+
+		var boxes = document.getElementsByClassName(classname);
+		for (let box of boxes) {
+			if (box.offsetWidth != 0 && box.offsetHeight !=0){
+				box.checked = false;
+			}
+		}
+	},
+
+	ToggleSelect: function(element) {
+		if(element.checked == true){
+			this.SelectAll(element.id);
+		} else {
+			this.ClearAll(element.id);
+		}
+	},
+
+	EnableButtons: function() {
+		var indicators, geoyears, submitters, hasind, hasgeo;
+
+		indicators	= document.getElementsByName("indicator");
+		geoyears	= document.getElementsByName("geo_year");
+		submitters	= document.getElementsByClassName("submitter");
+		hasind		= false;
+		hasgeo		= false;
+
+		for (let ind of indicators) {
+			if(ind.checked == true){
+				hasind = true;
+				break;
+			}
+		}
+		for (let geo of geoyears) {
+			if(geo.checked == true){
+				hasgeo = true;
+				break;
+			}
+		}
+		if(hasind && hasgeo) {
+			for (let sub of submitters) {
+				sub.removeAttribute('disabled');
+			}
+		} else {
+			for (let sub of submitters) {
+				sub.setAttribute('disabled', 'true');
+			}
+		}
+
+	},
+
+	FilterList: function(element) {
+		var filt_list, item_text, filt_text;
+		filt_list = document.getElementsByClassName(element.dataset.filterclass);
+		filt_text = element.value.toUpperCase();
+		// Filter the lists to remove elements which don't match the filter. 
+		for (let item of filt_list) {
+			item_text = item.textContent || item.innerText;
+			if(item_text.toUpperCase().indexOf(filt_text) > -1 || filt_text == "") {
+				item.style.display = "block";
+			} else {
+				item.style.display = "none";
+			}
+
+		}
+		// Add/remove class for dealing with populated search boxes which do not
+		// have focus.
+		if(filt_text.length > 0){
+			element.classList.add('haschars');
+		} else if (filt_text.length == 0) {
+			element.classList.remove('haschars');
 		}
 	}
-	for(i=0; i < geoyears.length; i++) {
-		if(geoyears[i].checked == true){
-			hasgeo = true;
-			break;
-		}
-	}
-	if(hasind && hasgeo) {
-		for(i=0; i < submitters.length; i++) {
-			submitters[i].removeAttribute('disabled');
-		}
-	} else {
-		for(i=0; i < submitters.length; i++){
-			submitters[i].setAttribute('disabled', 'true');
-		}
-	}
 
-}
-
+});
